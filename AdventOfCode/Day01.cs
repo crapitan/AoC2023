de24349fea1596a5027974b3f442bded86a01106
Klas Broberg <klas.broberg@gmail.com>
2023-12-01 13:52:53 +0100

    feat: Solved Day1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,41 @@
-﻿namespace AdventOfCode;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
     private readonly string _input;
+    private List<string> lines;
 
     public Day01()
     {
         _input = File.ReadAllText(InputFilePath);
+        lines = _input.Split(System.Environment.NewLine).ToList();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        int value = 0;
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        foreach (var line in lines)
+        {
+            string numbers = new(line.Where(char.IsDigit).ToArray());
+            value += int.Parse($"{numbers[0]}{numbers[^1]}");
+        }
+
+        return new ValueTask<string>(value.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int value = 0;
+
+        foreach (var line in lines)
+        {
+            string numbers = line.FindAllNumbers();
+            value += int.Parse($"{numbers[0]}{numbers[^1]}");
+        }
+
+        return new ValueTask<string>(value.ToString());
+    }
 }
---
+++ AdventOfCode/Extensions/StringExtensions.cs
@@ -0,0 +1,32 @@
+namespace AdventOfCode;
+
+public static class StringExtensions
+{
+    public static string FindAllNumbers(this string input)
+    {
+        string[] numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+        string result = string.Empty;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (char.IsDigit(input[i]))
+            {
+                result += input[i];
+            }
+            else
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (input.Substring(i).StartsWith(numbers[j]))
+                    {
+                        // Little bit hacky
+                        result += (j + 1).ToString(); 
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
